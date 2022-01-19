186f61ca58df0f667d0283385e2befab4446bb7f
Emmzy17 <emmanueladjerry@gmail.com>
2022-01-19 14:05:07 -0800

    video_api

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse,abort
+from flask_restful import Api, Resource, reqparse,abort,fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 api = Api(app)
@@ -19,31 +19,40 @@ db.create_all()
 
 
 
+
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type = str, help = "Name of video", required= True )
 video_put_args.add_argument("likes", type = int, help = "Input value for Likes of video", required=True)
 video_put_args.add_argument("views", type = int, help = "Views of video", required = True)
 
-videos = {}
+resource_field = {
+    'id' : fields.Integer,
+    'name' : fields.String,
+    'views' : fields.Integer,
+    'like' : fields.Integer
+}
 
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message = "could not find any video")
-def abort_if_video_exist(video_id):
-    if video_id in videos:
-        abort(409, message = "Video already exist")
 
 class video(Resource):
+    @marshal_with(resource_field)
     def get(self, video_id):
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id = video_id).first()
+        if not result:
+            abort(404, message= 'Video not find ')
+        return result
     def put(self, video_id):
-        abort_if_video_exist(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return {video_id:args}
+        result = VideoModel.query.filter_by(id = video_id)
+        if result:
+            abort(409, message= 'Video i.d Taken')
+
+        video = VideoModel(id = video_id, name = args['name'], views = args['views'], likes = args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
     def delete(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        del videos[video_id]
+        #abort_if_video_id_doesnt_exist(video_id)
+        #del videos[video_id]
         return '', 204
 api.add_resource(video, '/video/<int:video_id>')
 
